d08e80ea5bd68f75501863affe70f39fe62ac959
1Light <redclover1.618@.com>
2023-12-13 15:40:43 -0800

    upload

---
+++ errors.c
@@ -0,0 +1,85 @@
+#include "shell.h"
+
+/**
+ *_eputs - Outputs the input string
+ * @str: The outputed string
+ *
+ * Return: Nothing
+ */
+void _eputs(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _eputchar - Gives the character c to stderr
+ * @c: The printed character
+ *
+ * Return: 1 for success
+ * For error, -1 is returned, and errno is set appropriately.
+ */
+int _eputchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putfd - Gives the character c to given fd
+ * @c: The printed character
+ * @fd: The filedescriptor considered
+ *
+ * Return: 1 for success.
+ * For error, -1 is outputted, and errno is set appropriately.
+ */
+int _putfd(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ *_putsfd - Outputs the input string
+ * @str: The printed string
+ * @fd: The filedescriptor considered
+ *
+ * Return: The number of chars used
+ */
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
+}
