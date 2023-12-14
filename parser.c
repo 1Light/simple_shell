b1b7f17feab25f4ab58add1f0f1e0dac2f83d608
Natadkab <nehemiahadane@gmail.com>
2023-12-13 16:08:54 -0800

    update

---
+++ parser.c
@@ -0,0 +1,86 @@
+#include "shell.h"
+
+/**
+ * is_cmd - Indicates if a file is an executable command
+ * @info: The information of the structure
+ * @path: The path of the file
+ *
+ * Return: 1 for true, or else 0
+ */
+int is_cmd(info_t *info, char *path)
+{
+	struct stat st;
+
+	(void)info;
+	if (!path || stat(path, &st))
+		return (0);
+
+	if (st.st_mode & S_IFREG)
+	{
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * dup_chars - multiplies characters
+ * @pathstr: The path string
+ * @start: The initial index
+ * @stop: The final index
+ *
+ * Return: The pointer to the new buffer
+ */
+char *dup_chars(char *pathstr, int start, int stop)
+{
+	static char buf[1024];
+	int i = 0, k = 0;
+
+	for (k = 0, i = start; i < stop; i++)
+		if (pathstr[i] != ':')
+			buf[k++] = pathstr[i];
+	buf[k] = 0;
+	return (buf);
+}
+
+/**
+ * find_path - Locates this cmd in the path string
+ * @info: The information of the structure
+ * @pathstr: The path string
+ * @cmd: The cmd to be determined
+ *
+ * Return: full path of cmd if found or NULL
+ */
+char *find_path(info_t *info, char *pathstr, char *cmd)
+{
+	int i = 0, curr_pos = 0;
+	char *path;
+
+	if (!pathstr)
+		return (NULL);
+	if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
+	{
+		if (is_cmd(info, cmd))
+			return (cmd);
+	}
+	while (1)
+	{
+		if (!pathstr[i] || pathstr[i] == ':')
+		{
+			path = dup_chars(pathstr, curr_pos, i);
+			if (!*path)
+				_strcat(path, cmd);
+			else
+			{
+				_strcat(path, "/");
+				_strcat(path, cmd);
+			}
+			if (is_cmd(info, path))
+				return (path);
+			if (!pathstr[i])
+				break;
+			curr_pos = i;
+		}
+		i++;
+	}
+	return (NULL);
+}
