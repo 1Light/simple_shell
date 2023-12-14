2f0f824ea00d892b8b12fb2741460c255f0ddd1c
Natadkab <nehemiahadane@gmail.com>
2023-12-13 16:03:53 -0800

    update

---
+++ string1.c
@@ -0,0 +1,87 @@
+#include "shell.h"
+
+/**
+ * _strcpy - Makes a clone of a string
+ * @dest: The final place
+ * @src: The input reservoir
+ *
+ * Return: The pointer to the final place
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	if (dest == src || src == 0)
+		return (dest);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
+
+/**
+ * _strdup - Multiplies a string
+ * @str: The multiplied string
+ *
+ * Return: The pointer to the multiplied string
+ */
+char *_strdup(const char *str)
+{
+	int length = 0;
+	char *ret;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str++)
+		length++;
+	ret = malloc(sizeof(char) * (length + 1));
+	if (!ret)
+		return (NULL);
+	for (length++; length--;)
+		ret[length] = *--str;
+	return (ret);
+}
+
+/**
+ *_puts - Outputs an input string
+ *@str: The string to be outputted
+ *
+ * Return: NULL
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _putchar - Gives the char c to stdout
+ * @c: The char to output
+ *
+ * Return: 1 for success.
+ * for error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
